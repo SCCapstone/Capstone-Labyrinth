6533b6efc4a32044501cde08d253c499d94331ed
SamDunny <samdunny.edu11@gmail.com>
2020-11-09 15:15:46 -0500

    Closes #10

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -39,6 +39,7 @@ int main() {
     float deltaTime = 0.0f;
     sf::Clock clock;
 
+    // start of game loop
     while(window.isOpen()) {
     
         // used in the update function
@@ -58,108 +59,8 @@ int main() {
         player.Draw(window);
         window.display();
     }
-
-
-
-
+    // end of game loop
 
 
     return 0;
 }
-
-
-
-
-
-
-/*
-
-// instantiate player object
-    Player main_character("imgs/forward_1.png");
-
-    // making player view (to maintain center-screen)
-    View player_view(Vector2f(0.0f,0.0f), Vector2f(float(window_width), float(window_height)));
-
-
-
-    // tree texture
-    sf::Texture tree_text;
-    tree_text.loadFromFile("imgs/trees_chest.png");
-    if (!tree_text.loadFromFile("imgs/trees_chest.png")) {
-        std::cout << "ERROR tree" << std::endl;
-    }
-
-    // creating tree sprite
-    sf::Sprite tree;
-    tree.setTexture(tree_text);
-    tree.setOrigin(36, 37);
-    tree.setPosition(750, 350);
-    tree.setScale(4.0,4.0);
-
-
-
-
-    // Main Loop
-    while (window.isOpen()) {
-
-        Event event;
-
-        // Event Loop
-        while (window.pollEvent(event)) {
-            switch(event.type) {
-                case Event::Closed:
-                    window.close();
-            }
-        }
-
-        player_view.setCenter(main_character.getPlayerPos());
-
-        window.clear(sf::Color::White);
-        window.setView(player_view);
-        main_character.drawPlayer(window);
-        window.draw(tree);
-        window.display();
-
-        while (Keyboard::isKeyPressed(Keyboard::Up)) {
-            main_character.movePlayer('u', speed);
-            player_view.setCenter(main_character.getPlayerPos());
-            window.clear(sf::Color::White);
-            window.setView(player_view);
-            main_character.drawPlayer(window);
-            window.draw(tree);
-            window.display();
-        }
-
-        while (Keyboard::isKeyPressed(Keyboard::Down)) {
-            main_character.movePlayer('d', speed);
-            player_view.setCenter(main_character.getPlayerPos());
-            window.clear(sf::Color::White);
-            window.setView(player_view);
-            main_character.drawPlayer(window);
-            window.draw(tree);
-            window.display();
-        }
-        
-        while (Keyboard::isKeyPressed(Keyboard::Left)) {
-            main_character.movePlayer('l', speed);
-            player_view.setCenter(main_character.getPlayerPos());
-            window.clear(sf::Color::White);
-            window.setView(player_view);
-            main_character.drawPlayer(window);
-            window.draw(tree);
-            window.display();
-        }
-
-        while (Keyboard::isKeyPressed(Keyboard::Right)) {
-            main_character.movePlayer('r', speed);
-            player_view.setCenter(main_character.getPlayerPos());
-            window.clear(sf::Color::White);
-            window.setView(player_view);
-            main_character.drawPlayer(window);
-            window.draw(tree);
-            window.display();
-        }
-
-    }
-
-*/
